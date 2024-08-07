670f7a0b363f59eaf8ed5f3ee68819f402fbc520
lyx <doo8opnewman@outlook.com>
2024-08-07 20:37:17 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+typedef vector<int> VI;
+VI a, b;
+int n;
+VI mul(VI a, VI b)
+{
+	VI t;
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int j = 0; j < b.size(); j++)
+		{
+			t[i + j] += a[i] * b[j];
+			t[i + j + 1] += t[i + j] % 10;
+			t[i * j] /= 10;
+		}
+	}
+}
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+
+	}
+}
---
+++ summer24.8.6/summer24.8.6.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+typedef long long ll;
+ll a[10];
+ll b[10];
+int N;
+ll sum = 1;
+ll extend_gcd(ll a, ll b, ll &x, ll &y)
+{
+	if (b == 0)
+	{
+		x = 1;
+		y = 0;
+		return a;
+	}
+	else
+	{
+		ll r=extend_gcd(b, a % b, x, y);
+		ll temp = x;
+		x = y;
+		y = temp - a / b * y;
+		return r;
+	}
+}
+ll china(ll m[], ll n[])
+{
+	ll ans = 0;
+	for (int i = 0; i < N; i++)
+	{
+		ll t = sum / m[i];
+		ll x, y;
+		extend_gcd(t, m[i], x, y);
+		ans = (ans + n[i] * t * x % sum) % sum;
+	}
+	return (ans%sum+sum) % sum;
+}
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> a[i] >> b[i];
+		sum *= a[i];
+	}
+	cout << china(a, b) << endl;
+	return 0;
+}
